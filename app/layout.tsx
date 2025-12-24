d142466266635b2009a0936ef813e5a392d9417c
neaL367 <atichatfantasy@gmail.com>
2025-12-24 12:29:59 +0700

    fix: modify metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,13 +13,13 @@ const geistSans = Geist({
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
-  title: "Neal367 photos",
+  title: "Neal367's photos",
   description: "a collection of my favorite memories.❣️",
   openGraph: {
-    title: "Neal367 photo",
+    title: "Neal367's photos",
     description: "a collection of my favorite memories.❣️",
     url: baseUrl,
-    siteName: "Neal367 photo",
+    siteName: "Neal367's photos",
     locale: "en_US",
     type: "website",
     images: [
@@ -27,13 +27,13 @@ export const metadata: Metadata = {
         url: `${baseUrl}/opengraph-image.jpg`,
         width: 1200,
         height: 630,
-        alt: "Neal367 photo",
+        alt: "Neal367's photos",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Neal367 photo",
+    title: "Neal367's photos",
     description: "a collection of my favorite memories.❣️",
     creator: "@NL367",
     images: `${baseUrl}/opengraph-image.jpg`,
